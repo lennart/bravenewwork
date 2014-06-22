efc263532908ff059f9973733c96626acba3a3d7
Lennart Melzer <me@lmaa.name>
2014-06-22 19:45:02 +0200

    [++] dist again, for gh-pages

--- app/scripts/controllers/places.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-app
-    .controller('PlacesCtrl', function($rootScope, $scope, $sce, $routeParams, Places, $log, $window, $element, $location, $timeout, Geolocation, $document) {
-        $scope.nextSlideId = Places.indexToSlideId($scope.currentSlide + 1);
-        $scope.prevSlideId = Places.indexToSlideId($scope.currentSlide - 1);
-    })

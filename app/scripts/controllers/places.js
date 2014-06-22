b3d4d026235be3e1117038f0abb12f0fad69c29f
Aaron Israel <me@a0n.name>
2014-06-22 16:50:22 +0200

    trying to implement autoplay

--- app/scripts/controllers/places.js
+++ app/scripts/controllers/places.js
@@ -21,17 +21,51 @@ app
 
         $scope.currentSlide = idToSlideIndex($scope.slides, $routeParams.slide)
 
+        $scope.changeSlide = function(id) {
+            console.log(id);
+        }
+
+        $scope.nextSlide = function(id) {
+          $log.info('[places] click:next ', id)
+          var index = idToSlideIndex(slides, id)
+          var slide = slides[index+1]
+
+          if (slide) {
+            $log.debug('[places] going to next slide', slide.id)
+            // $rootScope.$emit('audio:stop:' + index);
+            // $scope.slide = slide;
+
+            // $location.path(slide.id);
+             $location.path(slide.id) 
+          }
+          else {
+            $log.debug('[places] there\'s no way further from', slides[index])
+          }
+        }
+
+        $scope.prevSlide = function(id) {
+          $log.info('[places] scroll:prev ', id)
+
+          var index = idToSlideIndex(slides, id)
+          var slide = slides[index-1]
+
+          if (slide) {
+            $log.debug('[places] going to prev slide', slide.id)
+            // $scope.slide = slide;
+            $location.path(slide.id);
+          }
+          else {
+            $log.debug('[places] there\'s no way back from', slides[index])
+          }
+        }
+
         $scope.nextSlideId = indexToSlideId($scope.slides, $scope.currentSlide + 1);
         $scope.prevSlideId = indexToSlideId($scope.slides, $scope.currentSlide - 1);
 
         $scope.slide = slides[$scope.currentSlide]
         $rootScope.$on('$routeUpdate', function(scope, next, current) {
-
+            console.log("route update", scope, next, current);
         })
-
-        $scope.$on('h6:click', function(event,id){
-            console.log("asdasd!");
-        });
 
         // $rootScope.$on('$routeChangeStart', function(scope, next, current) {
         //   $log.info('route change start', next, current, scope)
